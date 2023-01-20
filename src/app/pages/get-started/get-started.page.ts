6835a941faba4292b857e426cca7cb8ff86a62b9
Ericademola <ademolaadeymieric@gmail.com>
2023-01-21 00:22:35 +0100

    unneeded

---
+++ src/app/pages/get-started/get-started.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-started',
+  templateUrl: './get-started.page.html',
+  styleUrls: ['./get-started.page.scss'],
+})
+export class GetStartedPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
