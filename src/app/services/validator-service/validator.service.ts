15eb11c7d36271f4cf3fb5e5f9774b3fb1d31464
Lekens <arokoyuolalekan@gmail.com>
2023-01-30 23:16:43 +0100

    quick fix

---
+++ src/app/services/validator-service/validator.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {ISignupObject} from "../../interfaces/sign-up";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidatorService {
+
+  constructor() { }
+
+  public validateSignup(signupObject: ISignupObject) {
+    return !!(signupObject.username && signupObject.firstName && signupObject.lastName);
+
+  }
+
+  public validateLogin(loginObject: {password: string ; username: string}) {
+    return !!(loginObject.username && loginObject.password);
+  }
+}
