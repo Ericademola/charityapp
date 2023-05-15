e1a80261f5a7f282644e641383b5d39f223ab590
adeyemi ademola <ademolaadeyemieric@gmail.com>
2023-05-15 21:51:13 +0100

    saved reminders made to display on page load

--- src/app/pages/set-reminder/set-reminder.page.ts
+++ src/app/pages/set-reminder/set-reminder.page.ts
@@ -21,12 +21,16 @@ export class SetReminderPage implements OnInit {
     }
   ]
 
-  savings:any = [];
+  savings:any;
   //getSaving:any = {};
 
   constructor(private navCtrl: NavController, private currentUserService: CurrentUserService) { };
 
   ngOnInit() {
+    
+    const currentUser = this.currentUserService.getCurrentUser();
+    this.savings = currentUser.reminders;
+    
   }
 
   navBack() {
@@ -44,8 +48,8 @@ export class SetReminderPage implements OnInit {
     currentUser.reminders.push(getSaving);
     currentUser.reminders.sort((a:any, b:any) => { 
       
-    let d1 = new Date(a.date);
-    let d2 = new Date(b.date);
+      let d1 = new Date(a.date);
+      let d2 = new Date(b.date);
      
       if (d1.getMonth() > d2.getMonth()) {
         return 1;
@@ -56,7 +60,7 @@ export class SetReminderPage implements OnInit {
       } else if (d1.getDate() < d2.getDate()) {
         return -1;
       } else { return d1.getHours() - d2.getHours()} 
-      
+
     })  
 
     if (  members.map((member:any) => member.username).includes(currentUser.username) ) {
